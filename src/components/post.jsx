70c9cca34d78db864c73d3aa48a5f3b62d479961
Yvonne Ng <yvonne@tradegecko.com>
2018-04-30 09:04:30 +0800

    Post detail to show user name

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { observable } from 'mobx'
 import { inject, observer } from 'mobx-react'
 
 @inject('store')
+@observer
 export default class Post extends Component {
 
   static propTypes = {
     post: PropTypes.object.isRequired,
   }
+  @observable userName = ''
 
-  componentWillMount() {
-    if (!this.props.entry) { return }
+  async componentWillReceiveProps(nextProps) {
+    if (!nextProps.post.userId) { return }
 
     const { UserStore } = this.props.store
-    UserStore.showEntry(nextProps.entry.userId)
+    const user = await UserStore.findOrShowEntry(parseInt(nextProps.post.userId))
+    this.userName = user.name
   }
 
   render() {
@@ -21,6 +25,8 @@ export default class Post extends Component {
     return (
       <div className="post">
         <div className="post__title">{post.title}</div>
+        {this.userName &&
+          <div className="post_user">by {this.userName}</div>}
       </div>
     )
   }
